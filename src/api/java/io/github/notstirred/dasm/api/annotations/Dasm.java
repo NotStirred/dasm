ce0a193e5ea3020576aa64c52a9cbbebcd17b9ab
Tom Martin <tom.martin1239@gmail.com>
2024-02-07 19:40:25 +0000

    Make Dasm sets no longer have a default value

--- src/api/java/io/github/notstirred/dasm/api/annotations/Dasm.java
+++ src/api/java/io/github/notstirred/dasm/api/annotations/Dasm.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
 public @interface Dasm {
-    Class<?>[] value() default { };
+    Class<?>[] value();
 }
