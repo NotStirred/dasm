2bc0d15f3219acee937c943bf7ee36fcac87516f
Tom Martin <tom.martin1239@gmail.com>
2024-02-15 02:50:40 +0000

    Fix test redirecting varargs parameter unintentionally

--- src/test/java/io/github/notstirred/dasm/test/tests/t2/T2Dasm.java
+++ src/test/java/io/github/notstirred/dasm/test/tests/t2/T2Dasm.java
@@ -10,12 +10,12 @@ import io.github.notstirred.dasm.api.annotations.transform.TransformFromMethod;
 
 @Dasm(T2Dasm.T2Set.class)
 public class T2Dasm {
-    @TransformFromMethod(value = @MethodSig("method1(Ljava/lang/Object;)V"))
+    @TransformFromMethod(value = @MethodSig("method1(Ljava/lang/Float;)V"))
     native String method1(String param);
 
     @RedirectSet
     public interface T2Set {
-        @TypeRedirect(from = @Ref(Object.class), to = @Ref(String.class))
+        @TypeRedirect(from = @Ref(Float.class), to = @Ref(String.class))
         abstract class A {
         }
     }
--- src/test/java/io/github/notstirred/dasm/test/tests/t2/T2Output.java
+++ src/test/java/io/github/notstirred/dasm/test/tests/t2/T2Output.java
@@ -1,7 +1,7 @@
 package io.github.notstirred.dasm.test.tests.t2;
 
 public class T2Output {
-    public void method1(Object a) {
+    public void method1(Float a) {
         int hashCode = a.hashCode();
         System.out.printf("thing foo %d\n", hashCode);
     }
