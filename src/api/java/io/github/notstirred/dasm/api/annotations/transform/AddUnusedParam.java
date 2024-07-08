ed3dd7aa24fdaa126baf4257d60f31289cf4f649
Tom Martin <tom.martin1239@gmail.com>
2024-07-08 21:32:04 +0100

    Release 2.2.0, Add documentation and example to @AddUnusedParam.

--- src/api/java/io/github/notstirred/dasm/api/annotations/transform/AddUnusedParam.java
+++ src/api/java/io/github/notstirred/dasm/api/annotations/transform/AddUnusedParam.java
@@ -5,6 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Can be added to a parameter in a method which is annotated with {@link TransformMethod} or {@link TransformFromMethod}
+ * <br/><br/>
+ * Any number of parameters can be added in any position, but the original parameters must exist somewhere in the
+ * signature, with their order unchanged.
+ * <br/><br/>
+ * <h2>Examples:</h2>
+ * <pre>{@code
+ * Here a string is inserted between the two original integers in the method signature.
+ * @TransformFromMethod(value = @MethodSig("method1(II)V"))
+ * public native float[] method1out1(int a, @AddUnusedParam String c, int b);
+ * }</pre>
+ */
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.CLASS)
 public @interface AddUnusedParam {
