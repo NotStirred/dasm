616107843a7eec1e463f7ccb3f57c2a0f623bd52
Tom Martin <tom.martin1239@gmail.com>
2024-08-31 21:16:15 +0100

    test: Add redirect chaining tests

---
+++ src/test/java/io/github/notstirred/dasm/test/tests/redirect_chaining_swap/RedirectChainingSwapInput.java
@@ -0,0 +1,14 @@
+package io.github.notstirred.dasm.test.tests.redirect_chaining_swap;
+
+import io.github.notstirred.dasm.test.targets.CubePos;
+import io.github.notstirred.dasm.test.targets.Vec3i;
+
+public class RedirectChainingSwapInput {
+    public void method1(Vec3i a) {
+        int val = a.x;
+
+        CubePos cubePos = new CubePos(5, 3, 2);
+
+        System.out.printf("thing foo %d\n", val);
+    }
+}
---
+++ src/test/java/io/github/notstirred/dasm/test/tests/redirect_chaining_swap/TestRedirectChainingSwap.java
@@ -0,0 +1,34 @@
+package io.github.notstirred.dasm.test.tests.redirect_chaining_swap;
+
+import io.github.notstirred.dasm.api.annotations.Dasm;
+import io.github.notstirred.dasm.api.annotations.redirect.redirects.TypeRedirect;
+import io.github.notstirred.dasm.api.annotations.redirect.sets.RedirectSet;
+import io.github.notstirred.dasm.api.annotations.selector.MethodSig;
+import io.github.notstirred.dasm.api.annotations.selector.Ref;
+import io.github.notstirred.dasm.api.annotations.transform.TransformFromMethod;
+import io.github.notstirred.dasm.test.targets.CubePos;
+import io.github.notstirred.dasm.test.targets.Vec3i;
+import io.github.notstirred.dasm.test.tests.BaseMethodTest;
+
+import static io.github.notstirred.dasm.test.tests.TestData.single;
+
+@Dasm(TestRedirectChainingSwap.Set.class)
+public class TestRedirectChainingSwap extends BaseMethodTest {
+    public TestRedirectChainingSwap() {
+        super(single(RedirectChainingSwapInput.class, RedirectChainingSwapOutput.class, TestRedirectChainingSwap.class));
+    }
+
+    @TransformFromMethod(value = @MethodSig("method1(Lio/github/notstirred/dasm/test/targets/Vec3i;)V"))
+    native void method1out(Vec3i param);
+
+    @RedirectSet
+    public interface Set {
+        @TypeRedirect(from = @Ref(Vec3i.class), to = @Ref(CubePos.class))
+        abstract class Vec3i_to_CubePos {
+        }
+
+        @TypeRedirect(from = @Ref(CubePos.class), to = @Ref(Vec3i.class))
+        abstract class CubePos_to_Vec3i {
+        }
+    }
+}
