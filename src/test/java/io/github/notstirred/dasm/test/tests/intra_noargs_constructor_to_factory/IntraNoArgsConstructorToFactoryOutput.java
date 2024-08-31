916613c322bcf5932ca8502645bc22dfe7eb3b8f
Tom Martin <tom.martin1239@gmail.com>
2024-08-31 21:16:40 +0100

    test: Add intra constructor to factory tests

---
+++ src/test/java/io/github/notstirred/dasm/test/tests/intra_noargs_constructor_to_factory/IntraNoArgsConstructorToFactoryOutput.java
@@ -0,0 +1,13 @@
+package io.github.notstirred.dasm.test.tests.intra_noargs_constructor_to_factory;
+
+import io.github.notstirred.dasm.test.targets.Soup;
+
+public class IntraNoArgsConstructorToFactoryOutput {
+    public Soup method1() {
+        return new Soup();
+    }
+
+    public Soup method1out() {
+        return Soup.create();
+    }
+}
---
+++ src/test/java/io/github/notstirred/dasm/test/tests/intra_onearg_constructor_to_factory/IntraOneArgConstructorToFactoryInput.java
@@ -0,0 +1,9 @@
+package io.github.notstirred.dasm.test.tests.intra_onearg_constructor_to_factory;
+
+import io.github.notstirred.dasm.test.targets.Soup;
+
+public class IntraOneArgConstructorToFactoryInput {
+    public Soup method1() {
+        return new Soup(5);
+    }
+}
