2535b3bbae7342590a742ae6df32e558bc857800
Tom Martin <tom.martin1239@gmail.com>
2024-09-19 16:44:19 +0100

    test: Add additional hashcode call to ensure incorrect calls are not redirected.

--- src/test/java/io/github/notstirred/dasm/test/tests/intra_method_redirect/IntraMethodRedirectInput.java
+++ src/test/java/io/github/notstirred/dasm/test/tests/intra_method_redirect/IntraMethodRedirectInput.java
@@ -2,6 +2,7 @@ package io.github.notstirred.dasm.test.tests.intra_method_redirect;
 
 public class IntraMethodRedirectInput {
     public void method1(String a) {
+        int i = new Object().hashCode();
         int val = a.hashCode();
         System.out.printf("thing foo %d\n", val);
     }
--- src/test/java/io/github/notstirred/dasm/test/tests/intra_method_redirect/IntraMethodRedirectOutput.java
+++ src/test/java/io/github/notstirred/dasm/test/tests/intra_method_redirect/IntraMethodRedirectOutput.java
@@ -2,11 +2,13 @@ package io.github.notstirred.dasm.test.tests.intra_method_redirect;
 
 public class IntraMethodRedirectOutput {
     public void method1(String a) {
+        int i = new Object().hashCode();
         int val = a.hashCode();
         System.out.printf("thing foo %d\n", val);
     }
 
     public void method1out(String a) {
+        int i = new Object().hashCode();
         int val = a.length();
         System.out.printf("thing foo %d\n", val);
     }
