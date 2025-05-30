6a448b1f3610d182515822a38f04be133f9fbb32
Tom Martin <tom.martin1239@gmail.com>
2025-05-30 12:57:37 +0100

    parser: Implement transform inheritance

--- src/main/java/io/github/notstirred/dasm/annotation/parse/RedirectSetImpl.java
+++ src/main/java/io/github/notstirred/dasm/annotation/parse/RedirectSetImpl.java
@@ -3,6 +3,9 @@ package io.github.notstirred.dasm.annotation.parse;
 import io.github.notstirred.dasm.annotation.parse.redirects.*;
 import io.github.notstirred.dasm.annotation.parse.redirects.FieldRedirectImpl.FieldMissingFieldRedirectAnnotationException;
 import io.github.notstirred.dasm.api.annotations.redirect.sets.RedirectSet;
+import io.github.notstirred.dasm.api.provider.MappingsProvider;
+import io.github.notstirred.dasm.data.ClassField;
+import io.github.notstirred.dasm.data.ClassMethod;
 import io.github.notstirred.dasm.exception.DasmAnnotationException;
 import io.github.notstirred.dasm.exception.DasmException;
 import io.github.notstirred.dasm.exception.NoSuchTypeExists;
@@ -11,8 +14,10 @@ import io.github.notstirred.dasm.exception.wrapped.DasmExceptionData;
 import io.github.notstirred.dasm.exception.wrapped.DasmFieldExceptions;
 import io.github.notstirred.dasm.exception.wrapped.DasmMethodExceptions;
 import io.github.notstirred.dasm.util.ClassNodeProvider;
-import lombok.Data;
+import io.github.notstirred.dasm.util.TypeUtil;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.*;
 
@@ -85,15 +90,14 @@ public class RedirectSetImpl {
             DasmClassExceptions innerClassExceptions = redirectSetExceptions.addNested(new DasmClassExceptions(
                     "An exception occurred when parsing inner class of redirect set", innerClassNode));
 
-            parseInnerClass(innerClassNode, innerClassExceptions).ifPresent(data -> {
-                innerClassData.put(innerClass.name, data);
-            });
+            parseInnerClass(innerClassNode, innerClassExceptions).ifPresent(data -> innerClassData.put(innerClass.name, data));
         }
 
-        if (redirectSetExceptions.hasWrapped()) {
-            exceptions.addNested(redirectSetExceptions);
-            return Optional.empty();
-        }
+        List<Node<InnerClassParsedData>> roots = createTopDownTree(innerClassData);
+
+        roots.forEach(root ->
+                root.children.forEach(child ->
+                        createInheritedRedirectsForTree(root.value, child)));
 
         Set<FieldToMethodRedirectImpl> fieldToMethodRedirects = new HashSet<>();
         Set<ConstructorToFactoryRedirectImpl> constructorToFactoryRedirects = new HashSet<>();
@@ -109,11 +113,82 @@ public class RedirectSetImpl {
             typeRedirects.addAll(data.typeRedirects);
         });
 
+        if (redirectSetExceptions.hasWrapped()) {
+            exceptions.addNested(redirectSetExceptions);
+            return Optional.empty();
+        }
+
         return Optional.of(new RedirectSetImpl(superRedirectSets, fieldToMethodRedirects, constructorToFactoryRedirects, fieldRedirects, methodRedirects, typeRedirects));
     }
 
-    @Data
+    /**
+     * Takes all of the inner classes of a redirect set and arranges them in a tree by their class hierarchy
+     */
+    private static @NotNull List<Node<InnerClassParsedData>> createTopDownTree(Map<String, InnerClassParsedData> innerClassData) {
+        List<Node<InnerClassParsedData>> roots = new ArrayList<>();
+        List<Node<InnerClassParsedData>> remaining = new ArrayList<>();
+        Map<String, Node<InnerClassParsedData>> nodes = new HashMap<>();
+
+        // Create initial data
+        innerClassData.forEach((name, data) -> {
+            Node<InnerClassParsedData> node = new Node<>(data);
+            nodes.put(name, node);
+            if (data.superDataName == null || data.superDataName.equals(TypeUtil.classToInternalName(Object.class))) {
+                roots.add(node);
+            } else {
+                remaining.add(node);
+            }
+        });
+
+        // Arrange into tree
+        while (!remaining.isEmpty()) {
+            for (Iterator<Node<InnerClassParsedData>> iterator = remaining.iterator(); iterator.hasNext(); ) {
+                Node<InnerClassParsedData> node = iterator.next();
+
+                Node<InnerClassParsedData> superNode = nodes.get(node.value.superDataName);
+                if (superNode != null) {
+                    superNode.children.add(node);
+                    iterator.remove();
+                }
+            }
+        }
+        return roots;
+    }
+
+    private static void createInheritedRedirectsForTree(InnerClassParsedData parent, Node<InnerClassParsedData> data) {
+        // FIXME: how should mappings owner interact with inheritance changing the owner?
+        //        is the current approach fine? the mappings owner never gets changed assuming it's a super type. feels weird but maybe correct.
+        OwnerChanger ownerChanger = new OwnerChanger(
+                parent.srcType.getClassName(), data.value.srcType.getClassName(),
+                parent.dstType.getClassName(), data.value.dstType.getClassName()
+        );
+        parent.fieldToMethodRedirects.stream().map(ownerChanger::remap).forEach(r -> data.value.fieldToMethodRedirects.add(r));
+        parent.constructorToFactoryRedirects.stream().map(ownerChanger::remap).forEach(r -> data.value.constructorToFactoryRedirects.add(r));
+        parent.fieldRedirects.stream().map(ownerChanger::remap).forEach(r -> data.value.fieldRedirects.add(r));
+        parent.methodRedirects.stream().map(ownerChanger::remap).forEach(r -> data.value.methodRedirects.add(r));
+        parent.typeRedirects.stream().map(ownerChanger::remap).forEach(r -> data.value.typeRedirects.add(r));
+
+        data.value.fieldToMethodRedirects.addAll(parent.fieldToMethodRedirects);
+        data.value.constructorToFactoryRedirects.addAll(parent.constructorToFactoryRedirects);
+        data.value.fieldRedirects.addAll(parent.fieldRedirects);
+        data.value.methodRedirects.addAll(parent.methodRedirects);
+        data.value.typeRedirects.addAll(parent.typeRedirects);
+
+        data.children.forEach(child -> createInheritedRedirectsForTree(data.value, child));
+    }
+
+    @RequiredArgsConstructor
+    private static class Node<T> {
+        final T value;
+        final List<Node<T>> children = new ArrayList<>();
+    }
+
     private static class InnerClassParsedData {
+        Type srcType;
+        Type dstType;
+
+        String superDataName;
+
         Set<FieldToMethodRedirectImpl> fieldToMethodRedirects = new HashSet<>();
         Set<ConstructorToFactoryRedirectImpl> constructorToFactoryRedirects = new HashSet<>();
         Set<FieldRedirectImpl> fieldRedirects = new HashSet<>();
@@ -180,6 +255,10 @@ public class RedirectSetImpl {
                 return Optional.empty();
             }
         }
+
+        data.srcType = srcType[0];
+        data.dstType = dstType[0];
+        data.superDataName = innerClassNode.superName;
 
         parseFields(innerClassNode, srcType[0], dstType[0],
                 data.fieldRedirects,
@@ -275,6 +354,83 @@ public class RedirectSetImpl {
             methodRedirect.ifPresent(methodRedirects::add);
             fieldToMethodRedirect.ifPresent(fieldToMethodRedirects::add);
             constructorToFactoryRedirect.ifPresent(constructorToFactoryRedirects::add);
+        }
+    }
+
+    private static class OwnerChanger implements MappingsProvider {
+        private final Map<String, String> typeMapping = new HashMap<>();
+
+        OwnerChanger(String oldSrc, String newSrc, String oldDst, String newDst) {
+            this.typeMapping.put(oldSrc, newSrc);
+            this.typeMapping.put(oldDst, newDst);
+        }
+
+        @Override
+        public String mapFieldName(String owner, String fieldName, String descriptor) {
+            return fieldName;
+        }
+
+        @Override
+        public String mapMethodName(String owner, String methodName, String descriptor) {
+            return methodName;
+        }
+
+        @Override
+        public String mapClassName(String className) {
+            return typeMapping.getOrDefault(className, className);
+        }
+
+        private ClassField remap(ClassField classField) {
+            return new ClassField(this.remapType(classField.owner()), this.remapType(classField.mappingsOwner()), classField.type(), classField.name());
+        }
+
+        private ClassMethod remap(ClassMethod classMethod) {
+            return new ClassMethod(this.remapType(classMethod.owner()), this.remapType(classMethod.mappingsOwner()), classMethod.method());
+        }
+
+        public FieldToMethodRedirectImpl remap(FieldToMethodRedirectImpl redirect) {
+            return new FieldToMethodRedirectImpl(
+                    this.remap(redirect.srcField()),
+                    this.remap(redirect.getterDstMethod()),
+                    redirect.setterDstMethod().map(this::remap),
+                    redirect.isStatic(),
+                    redirect.isDstOwnerInterface()
+            );
+        }
+
+        public ConstructorToFactoryRedirectImpl remap(ConstructorToFactoryRedirectImpl redirect) {
+            return new ConstructorToFactoryRedirectImpl(
+                    this.remap(redirect.srcConstructor()),
+                    this.remapType(redirect.dstOwner()),
+                    redirect.dstName(),
+                    redirect.isDstOwnerInterface()
+            );
+        }
+
+        public FieldRedirectImpl remap(FieldRedirectImpl redirect) {
+            return new FieldRedirectImpl(
+                    this.remap(redirect.srcField()),
+                    this.remapType(redirect.dstOwner()),
+                    redirect.dstName()
+            );
+        }
+
+        public MethodRedirectImpl remap(MethodRedirectImpl redirect) {
+            return new MethodRedirectImpl(
+                    this.remap(redirect.srcMethod()),
+                    this.remapType(redirect.dstOwner()),
+                    redirect.dstName(),
+                    redirect.isStatic(),
+                    redirect.isDstOwnerInterface()
+            );
+        }
+
+        public TypeRedirectImpl remap(TypeRedirectImpl redirect) {
+            return new TypeRedirectImpl(
+                    this.remapType(redirect.srcType()),
+                    this.remapType(redirect.dstType()),
+                    redirect.isDstInterface()
+            );
         }
     }
 
